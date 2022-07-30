0a79c374cabc9c7e0096bba2deadcc99dca00f25
ryanrasoarahona3 <rasoarahonarivoniaina@ku.th>
2022-07-30 10:34:15 +0700

    Use createPool instead of createConnection

--- src/service/DatabaseManager.js
+++ src/service/DatabaseManager.js
@@ -16,7 +16,7 @@ class DatabaseManager{
 
     async init(){
         return new Promise(((resolve, reject)=>{
-            this.#connection = mysql.createConnection({
+            this.#connection = mysql.createPool({
                 host: 'us-cdbr-east-06.cleardb.net',
                 user: 'b0d4addb6803a3',
                 password: '2dc05f71',
